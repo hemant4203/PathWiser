abbe666572cc084e9a2d95d5c5ea0614262f1ade
hemant4203 <85121389+hemant4203@users.noreply.github.com>
2026-03-10 18:13:15 +0530

    Add files via upload

---
+++ career-backend/src/main/java/com/career/backend/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.career.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://path-wiser.vercel.app",
+                                "http://localhost:5173"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
